8e89c853da4eb9aafc46d7640f274c5eedb0b89c
NjimsRane <njims2011@gmail.com>
2022-10-08 22:57:48 +0100

    done with the hero section

---
+++ App/JS/script.js
@@ -0,0 +1,21 @@
+const hamburgerMenu =document.querySelector(".hamburgerMenu");
+const hamburgerMenuChange =document.querySelector(".hamburgerMenuChange");
+const hero =document.querySelector(".hero");
+
+const navBar =document.querySelector(".navBar");
+
+const menuDishes =document.querySelector(".menuDishes");
+const dishes =document.querySelector(".dishes");
+
+
+
+
+hamburgerMenu.addEventListener("click",()=>{
+    hamburgerMenu.classList.toggle("hamburgerMenuChange");
+    hero.classList.toggle("heroChange");
+    navBar.classList.toggle("navBarChange");
+
+})
+.menuDishes.addEventListener("click",()=>{
+    dishes.classList.toggle("dishesChange")
+})
